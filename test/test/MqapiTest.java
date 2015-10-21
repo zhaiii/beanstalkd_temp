cad636f2abf40e7fbe3c21efeda95086340ee2a0
millions.zhai <millions.zhai@ishansong.com>
2015-10-21 21:11:35 +0800

    test client

--- test/test/MqapiTest.java
+++ test/test/MqapiTest.java
@@ -4,6 +4,7 @@ import com.ishansong.mqapi.IMqClient;
 import com.ishansong.mqapi.IMqClientFactory;
 import com.ishansong.mqapi.MqMessage;
 import org.junit.Test;
+import trendrr.beanstalk.BeanstalkJob;
 
 /**
  * Created by zhai on 15/10/21.
@@ -22,14 +23,35 @@ public class MqapiTest {
 //        String s = cleint.tubeStats();
 //        System.out.println(s);
 //        cleint.watchTube("default");
-        MqMessage reserve = mqClient.reserve(23);
-        System.out.println(new String(reserve.getData()));
+        MqMessage message = mqClient.reserve(23);
+        System.out.println(new String(message.getData()));
 
-        IMqClient cleint = reserve.getCleint();
+        IMqClient cleint = message.getCleint();
         String s = cleint.tubeStats();
         System.out.println(s);
 //        String s1 = mqClient.tubeStats();
 //        System.out.println(s1);
+
+    }
+    @Test
+    public void test1() throws Exception{
+        IMqClient mqClient = IMqClientFactory.newInstance().getMqClient("127.0.0.1", 11300);
+        mqClient.put(1l, 0, 5000, "this is some data".getBytes());
+        MqMessage reserve = mqClient.reserve(60);
+        System.out.println(new String(reserve.getData()));
+        mqClient.deleteJob(reserve.getId());
+        mqClient.close(); //closes the connection
+    }
+
+    @Test
+    public void test2() throws Exception{
+        IMqClient mqClient = IMqClientFactory.newInstance().getPooledMqClient("127.0.0.1", 11300, 10);
+        long put = mqClient.put(23, 0, 90, "gagag".getBytes());
+        System.out.println(put);
+        MqMessage mess = mqClient.reserve(90);
+        IMqClient cleint = mess.getCleint();
+        String s = cleint.tubeStats();
+        System.out.println(s);
 
     }
 }
